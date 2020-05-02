b7c0fcfce2ff9df02555c50aa64ac4c57d03ee27
Mateo balza <balzamateo5@gmail.com>
2020-05-02 12:10:49 -0500

    modifique los estilos

--- insertar.php
+++ insertar.php
@@ -7,16 +7,13 @@
       echo "error al conectar la DB";
     }
 
-    // if(isset($_POST['nombre']) && !empty($_POST['nombre']) && isset($_POST['pw']) && !empty($_POST['pw'])){
-    //
-    //     $conexion = mysqli_connect("localhost","root"," ","practica") or die("problemas con la conexión");
-    //     mysqli_select_db($conexion) or die("problemas con la conexión");
-    //
-    //     mysqli_query($conexion,"INSERT INTO datos (Nombre,Pw) values ('$_REQUEST[nombre]','$_REQUEST[pw]')");
-    //     echo "datos insertados corectamente";
-    //
-    // }else{
-    //     echo "Problemas al insertar datos";
-    // }
+     if(isset($_POST['nombre']) && !empty($_POST['nombre']) && isset($_POST['pw']) && !empty($_POST['pw'])){
+    
+         mysqli_query($conect,"INSERT INTO datos (Nombre,Pw) values ('$_POST[nombre]','$_POST[pw]')");
+         echo "datos insertados corectamente";
+    
+     }else{
+         echo "Problemas al insertar datos";
+     }
 
 ?>
